e59f12e24ca853f82dc5126bb4cbce23927d250c
Lasse <lassewolpmann@outlook.de>
2022-06-21 12:28:00 +0300

    Added components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AgentWheel />
+  <PageFooter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AgentWheel from "@/components/AgentWheel";
+import PageFooter from "@/components/PageFooter";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageFooter,
+    AgentWheel
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 100vh;
+  height: auto;
+  width: 100vw;
+
+  background: rgb(255,68,101);
+  background: linear-gradient(90deg, rgba(255, 68, 101, 1) 0%, rgba(50, 50, 50, 1) 100%);
 }
 </style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <a href=""></a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageFooter"
+}
+</script>
+
+<style scoped>
+  footer {
+    position: fixed;
+    bottom: 0;
+  }
+</style>
